15d1c4032f6144cff616aa02f74f49f2f18673d8
wriver4 <wriver4@gmail.com>
2024-12-05 12:20:57 -0500

    remove key

--- King/Module Version /modules/users.nix
+++ King/Module Version /modules/users.nix
@@ -9,7 +9,7 @@
       extraGroups = [ "networkmanager" "wheel" "libvirtd" "lxd" "systemd-journal" "podman"]; # "vbox" "vboxusers"
       openssh.authorizedKeys.keys = [
         # Replace with your own public key
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtBZ5F327lNczQ76KxK1ibJ8wl/cMh1R8DvZh/uB3LP mark@king"
+        ""
       ];    
     packages = with pkgs; [];
     };
